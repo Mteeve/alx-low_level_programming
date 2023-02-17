d6a8fcc6a0a8e469aa6397c71ca78d602077a697
Mteeve <mteeve@gmail.com>
2023-02-16 16:47:37 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,6 +10,6 @@
 int main(void)
 
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	fputs(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
