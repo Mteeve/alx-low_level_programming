406531854765505f07ae9a72a0eef44c09f8857e
mteeve <mteeve@gmail.com>
2023-04-04 08:40:58 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,32 +20,32 @@ listint_t *temp = *head;
 
 new = malloc(sizeof(listint_t));
 
-	if (!new || !head)
-	return (NULL);
+if (!new || !head)
+return (NULL);
 
-	new->n = n;
-	new->next = NULL;
-		
-	if (idx == 0)
-	{
-		new->next = *head;
-		*head = new;
-		return (new);
-	}
+new->n = n;
+new->next = NULL;
 
-		for (i = 0; temp && i < idx; i++)
+if (idx == 0)
+{
+new->next = *head;
+*head = new;
+return (new);
+}
+
+for (i = 0; temp && i < idx; i++)
 
 {
-		if (i == idx - 1)
-			{
-				new->next = temp->next;
-				temp->next = new;
-				
-				return (new);
-			}
+if (i == idx - 1)
+{
+new->next = temp->next;
+temp->next = new;
 
-		else
-		temp = temp->next;
+return (new);
+}
+
+else
+temp = temp->next;
 }
 
 return (NULL);
