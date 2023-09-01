4f7da1e3e01dfe7a23aec7d675df51c49382ea51
Mteeve <mteeve@gmail.com>
2023-09-01 10:33:33 -0700

    5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,26 +1,28 @@
 #include "main.h"
 
 /**
- * flip_bits - counts the number of bits to change
- * to get from one number to another
- * @n: first number
- * @m: second number
+ * flip_bits - Counts the number of bits to change
+ * to get from one number to another.
  *
- * Return: number of bits to change
+ * @num1: First number.
+ * @num2: Second number.
+ *
+ * Return: Number of bits to change.
  */
-unsigned int flip_bits(unsigned long int n, unsigned long int m)
+unsigned int flip_bits(unsigned long int num1, unsigned long int num2)
 {
-	int i, count = 0;
-	unsigned long int current;
-	unsigned long int exclusive = n ^ m;
+    int i, count = 0;
+    unsigned long int current;
+    unsigned long int xor_result = num1 ^ num2;
 
-	for (i = 63; i >= 0; i--)
-	{
-		current = exclusive >> i;
-		if (current & 1)
-			count++;
-	}
+    for (i = 63; i >= 0; i--)
+    {
+        current = xor_result >> i;
+        if (current & 1)
+        {
+            count++;
+        }
+    }
 
-	return (count);
+    return (count);
 }
-
